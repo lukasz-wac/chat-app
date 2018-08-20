b9569cf4b445cd5cb4ea8099024c413603eede29
lukasz.wac <lukasz.wac@naturaily.com>
2018-08-20 17:02:30 +0200

    emit new Message on create Message

--- server/server.js
+++ server/server.js
@@ -1,15 +1,34 @@
 require('./config/config');
 
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const PORT = process.env.PORT;
 
 const publicPath = path.join(__dirname, '..', 'public');
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
+io.on('connection', socket => {
+  console.log('new user connected');
+  socket.on('disconnect', () => {
+    console.log('disconnected from server');
+  });
+
+  socket.on('createMessage', message => {
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createAt: new Date().getTime()
+    });
+  });
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
