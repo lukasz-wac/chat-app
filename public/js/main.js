587d1c9b037bec8440f8a904a05b00859d6238f7
lukasz.wac <lukasz.wac@naturaily.com>
2018-08-21 13:04:24 +0200

    visual sending file

--- public/js/main.js
+++ public/js/main.js
@@ -9,5 +9,21 @@ socket.on('disconnect', () => {
 });
 
 socket.on('newMessage', message => {
-  console.log('New message arrived', message);
+  const li = $('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  $('#messages').append(li);
+});
+
+$('#message-form').on('submit', e => {
+  e.preventDefault();
+
+  socket.emit(
+    'createMessage',
+    {
+      from: 'User',
+      text: $('[name=message]').val()
+    },
+    () => {}
+  );
 });
--- server/server.js
+++ server/server.js
@@ -12,20 +12,30 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
+const { generateMessage } = require('./utils/message');
+
 app.use(express.static(publicPath));
 
 io.on('connection', socket => {
   console.log('new user connected');
+
+  socket.emit('newMessage', generateMessage('admin', 'welcome to the chat app!'));
+
+  socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined to the chat!'));
+
   socket.on('disconnect', () => {
     console.log('disconnected from server');
   });
 
-  socket.on('createMessage', message => {
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createAt: new Date().getTime()
-    });
+  socket.on('createMessage', (message, callback) => {
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback('This is from the Server');
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
   });
 });
 
